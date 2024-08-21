d728b6adc94cc214d6d0e612512531ce5e65eda6
Matheus Mattos <matheusbmtt@gmail.com>
2024-08-21 14:27:03 -0300

    Update 1.ts

--- AULA13/1.ts
+++ AULA13/1.ts
@@ -1,30 +1,67 @@
-class Contador {
-    private valorContador: number;
-  
-    constructor() {
-      this.valorContador = 0; // Inicializa o contador com valor zero
+export let rl = require("readline-sync")
+
+export interface Contador{
+    valorContador: number;
+    zerar(): void;
+    incrementar(): void;
+    valor(): number;
+}
+
+export class Contador implements Contador{
+    public valorContador: number;
+
+    constructor(valorContador: number){
+        this.valorContador = valorContador
     }
-  
-    // Método para zerar o contador
+
+    //Começo o contador zerando ele
     zerar(): void {
-      this.valorContador = 0;
+        this.valorContador = 0;
     }
-  
-    // Método para incrementar o contador
-    incrementar(): void {
-      this.valorContador += 1;
+
+    // Incrimento em um em um
+    public incrementar(): void {
+        this.valorContador += 1;
+        // OU this.valorContador = this.valorContador + this.valorContador 
     }
-  
-    // Método para obter o valor atual do contador
-    valor(): number {
-      return this.valorContador;
+    //Retorna o Valor atual do Contador
+    public valor(): number {
+        return this.valorContador
     }
-  }
-  
-  // Exemplo de uso
-  const meuContador = new Contador();
-  console.log(meuContador.valor()); // 0
-  meuContador.incrementar();
-  console.log(meuContador.valor()); // 1
-  meuContador.zerar();
-  console.log(meuContador.valor()); // 0
+}
+
+
+function criarContador(): Contador {
+    let valorCriado = rl.questionInt("Defina o valor inicial do contador: ");
+    return new Contador(valorCriado);
+}
+
+let contador1 = criarContador();
+
+let menu: boolean = true;
+while (menu) {
+    let opcao = rl.questionInt('Insira o numero da opcao desejada:\n1 - Definir Valor Inicial\n2 - Visualizar Contador\n3 - Incrementar Contador\n4 - Zerar Contador\n5 - Sair\n');
+
+    switch(opcao) {
+        case 1:
+            contador1 = criarContador();
+            break;
+        case 2:
+            console.log("Valor do Contador:", contador1.valor());
+            break;
+        case 3:
+            contador1.incrementar();
+            break;
+        case 4:
+            contador1.zerar();
+            break;
+        case 5:
+            menu = false;
+            break;
+        default:
+            console.log("Oppco invalida. Tente novamente.");
+            break;
+    }
+}
+
+console.log("Programa encerrado.");
